b525d2ffe6a5647c60fef16f9b8b127445167aab
prajakta1211 <67505663+prajakta1211@users.noreply.github.com>
2020-07-07 19:54:31 +0530

    add changes to initial files

---
+++ index.js
@@ -0,0 +1,53 @@
+
+for(var i=0;i<document.querySelectorAll(".drum").length;i++)
+{
+  document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+  var buttonInnerHTML= this.innerHTML;
+  makeSounds(buttonInnerHTML);
+  buttonAnimationCreator(buttonInnerHTML);
+  });
+}
+document.addEventListener("keydown",function(event){
+  var keypressed=event.key;
+  makeSounds(keypressed);
+  buttonAnimationCreator(keypressed);
+})
+function makeSounds(r){
+switch (r) {
+  case "d":
+  var tom1=new Audio("sounds/tom-1.mp3");
+  tom1.play();
+    break;
+    case "j":
+    var tom2=new Audio("sounds/tom-2.mp3");
+      tom2.play();
+    break;
+    case "k":
+    var tom3=new Audio("sounds/tom-3.mp3");
+    tom3.play();
+    break;
+    case "l":
+    var tom4=new Audio("sounds/tom-4.mp3");
+    tom4.play();
+    break;
+    case "w":
+    var crash=new Audio("sounds/crash.mp3");
+    crash.play();
+    break;
+    case "a":
+    var kick=new Audio("sounds/kick-bass.mp3");
+    kick.play();
+    break;
+    case "s":
+      var snare=new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+  default: console.log(buttonInnerHTML);
+}
+}
+function buttonAnimationCreator(bac){
+  document.querySelector("."+bac).classList.add("pressed");
+  setTimeout(function(){
+    document.querySelector("."+bac).classList.remove("pressed");
+  },100);
+}
